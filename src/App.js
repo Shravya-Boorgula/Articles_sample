6038b26c7da3845be5ca58ae9a958aa3313a8e74
sboorgula <sboorgula@ramsoft.com>
2020-02-28 19:15:36 +0530

    Cards UI using static data

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import Header from './components/Header';
+import ArticlesContainer from './/components/ArticlesContainer';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Header/>
-
+      <ArticlesContainer/>
     </div>
   );
 }
